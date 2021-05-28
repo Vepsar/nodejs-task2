dd7126e40c0d30eecf8450a2c73e8e9b9ff1730e
vepsar <vepsarvz@gmail.com>
2021-05-28 22:30:35 +0300

    feat: add description to board files

--- src/resources/boards/board.mem.repo.js
+++ src/resources/boards/board.mem.repo.js
@@ -1,33 +1,73 @@
-const boards = []
-const taskService = require('../tasks/task.service')
+const boards = [];
+const taskService = require('../tasks/task.service');
 
-const getAllBoards = async () => boards
-const getBoardById = async (id) => boards.find((board)=>board.id === id)
+/**
+ * Function for return Array of all boards in DB
+ * @returns {Promise<Board[]>}
+ * Return array of all boards from the DB
+ */
+
+const getAllBoards = async () => boards;
+
+/**
+ * Get id of the board and return this board when it exsist as the Object
+ * @param {String} id This is string with id of board
+ * @returns {Promise<Board>}
+ * Return Object by class Board
+ */
+const getBoardById = async (id) => boards.find((board) => board.id === id);
+
+/**
+ * Create board in DB from the receiving data
+ * @param {Object} data Object with class Board that contain all necessery information
+ * @returns {Promise<Board>}
+ * return created Object
+ */
 
 const createBoard = async (data) => {
-    boards.push(data)
-    return data
-}
+  boards.push(data);
+  return data;
+};
+
+/**
+ * Update board in DB by providing id and updated info
+ * @param {String} id id of the changing board
+ * @param {Object} data info that need to update
+ * @returns {Promise<Board>}
+ * Return updated object
+ */
 
 const updateBoard = async (id, data) => {
-    const idNum = boards.findIndex((board)=>board.id === id)
-    const updBrd = {
-        id,
-        title: data.title,
-        columns: data.columns
-    }
-    boards.splice(idNum, 1, updBrd)
-    return boards.find((board)=>board.id === id)
-}
+  const idNum = boards.findIndex((board) => board.id === id);
+  const updBrd = {
+    id,
+    title: data.title,
+    columns: data.columns,
+  };
+  boards.splice(idNum, 1, updBrd);
+  return boards.find((board) => board.id === id);
+};
+
+/**
+ * Delete board from DB by providing ID
+ * @param {String} id id of board that need to delete
+ */
 
 const deleteBoard = async (id) => {
-  const tasksid = await taskService.getAllTasks(id)
-  Promise.all(tasksid.map(async (task) =>{
-          await taskService.deleteTask(task.id)
-      }))
-  const idNum = boards.findIndex((board)=>board.id === id)
-  boards.splice(idNum, 1)
-}
+  const tasksid = await taskService.getAllTasks(id);
+  Promise.all(
+    tasksid.map(async (task) => {
+      await taskService.deleteTask(task.id);
+    })
+  );
+  const idNum = boards.findIndex((board) => board.id === id);
+  boards.splice(idNum, 1);
+};
 
-
-module.exports = {getAllBoards, getBoardById, createBoard, deleteBoard, updateBoard}
+module.exports = {
+  getAllBoards,
+  getBoardById,
+  createBoard,
+  deleteBoard,
+  updateBoard,
+};
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,9 +1,42 @@
-const boardRepo = require('./board.mem.repo')
+const boardRepo = require('./board.mem.repo');
+/**
+ * Service to get all boards
+ * @returns {Promise<Board[]>}
+ */
+const getAllBoards = () => boardRepo.getAllBoards();
 
-const getAllBoards = () => boardRepo.getAllBoards()
-const getBoardById = (id) => boardRepo.getBoardById(id)
-const createBoard = (data) => boardRepo.createBoard(data)
-const deleteBoard = (id) => boardRepo.deleteBoard(id)
-const updateBoard = (id, data) => boardRepo.updateBoard(id, data)
+/**
+ * Service for returning board by provided ID
+ * @param {String} id - board's ID
+ * @returns {Promise<Board>}
+ */
+const getBoardById = (id) => boardRepo.getBoardById(id);
 
-module.exports = { getAllBoards, getBoardById, createBoard, deleteBoard, updateBoard}
+/**
+ * Service for creating board by data
+ * @param {Board} data - board's data
+ * @returns {Promise<Board>}
+ */
+const createBoard = (data) => boardRepo.createBoard(data);
+
+/**
+ * Service to delete board by ID
+ * @param {String} id - ID of board that need to delete
+ */
+const deleteBoard = (id) => boardRepo.deleteBoard(id);
+
+/**
+ * Service to updating board bt providing ID
+ * @param {String} id - board's ID
+ * @param {Board} data - updating info
+ * @returns {Promise<Board>} - return updated object
+ */
+const updateBoard = (id, data) => boardRepo.updateBoard(id, data);
+
+module.exports = {
+  getAllBoards,
+  getBoardById,
+  createBoard,
+  deleteBoard,
+  updateBoard,
+};
